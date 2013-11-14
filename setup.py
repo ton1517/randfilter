ffca928b8e49c6b0f49ec4862ed097c84112bce7
ton <tonton1517@gmail.com>
2013-11-14 23:46:31 +0900

    add setup files.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import randfilter
+
+setup(
+    name = randfilter.NAME,
+    version = randfilter.VERSION,
+    description = randfilter.DESCRIPTION,
+    long_description = open("README.rst").read(),
+    url = randfilter.URL,
+    license = randfilter.LICENSE,
+    author = randfilter.AUTHOR,
+    author_email = randfilter.AUTHOR_EMAIL,
+    py_modules = ['randfilter'],
+    entry_points = {
+        "console_scripts": ["randfilter = randfilter:main"]
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Utilities',
+    ]
+)
+
